48df17ba1e83cf9a7490c306a946471bfc2ebae3
Mattis Megevand <megevand.mattis@gmail.com>
2024-07-07 01:28:04 +0200

    done

--- src/helper.zig
+++ src/helper.zig
@@ -9,20 +9,22 @@ pub fn degrees_to_radians(degrees: f64) f64 {
 
 pub const helper = undefined;
 
-var prng: std.Random.Xoshiro256 = undefined;
-var rand: std.Random = undefined;
+var prng: ?std.Random.Xoshiro256 = null;
+var rand: ?std.Random = null;
 
 pub fn random_init() !void {
-    prng = std.Random.DefaultPrng.init(blk: {
-        var seed: u64 = undefined;
-        try std.posix.getrandom(std.mem.asBytes(&seed));
-        break :blk seed;
-    });
-    rand = prng.random();
+    if (prng == null) {
+        prng = std.Random.DefaultPrng.init(blk: {
+            var seed: u64 = undefined;
+            try std.posix.getrandom(std.mem.asBytes(&seed));
+            break :blk seed;
+        });
+        rand = prng.?.random();
+    }
 }
 
 pub fn random_double() f64 {
-    return rand.float(f64);
+    return rand.?.float(f64);
 }
 
 pub fn random_double_range(min: f64, max: f64) f64 {
--- src/main.zig
+++ src/main.zig
@@ -17,32 +17,58 @@ pub fn main() !void {
     var world = hittable_list{ .objects = std.ArrayList(hittable).init(allocator) };
     defer world.objects.deinit();
 
-    const material_ground: material.material = material.material{ .lambertian = material.lambertian.init(color.color.init(0.8, 0.8, 0.0)) };
-    const material_center: material.material = material.material{ .lambertian = material.lambertian.init(color.color.init(0.1, 0.2, 0.5)) };
-    const material_left: material.material = material.material{ .dielectric = material.dielectric.init(1.50) };
-    const material_bubble: material.material = material.material{ .dielectric = material.dielectric.init(1.00 / 1.50) };
-    const material_right: material.material = material.material{ .metal = material.metal.init(color.color.init(0.8, 0.6, 0.2), 1.0) };
+    const ground_material: material.material = material.material{ .lambertian = material.lambertian.init(color.color.init(0.5, 0.5, 0.5)) };
+    try world.objects.append(hittable{ .sphere = sphere.init(point3.init(0, -1000, 0), 1000, ground_material) });
 
-    try world.objects.append(hittable{ .sphere = sphere.init(point3.init(0.0, -100.5, -1.0), 100.0, material_ground) });
-    try world.objects.append(hittable{ .sphere = sphere.init(point3.init(0.0, 0.0, -1.2), 0.5, material_center) });
-    try world.objects.append(hittable{ .sphere = sphere.init(point3.init(-1.0, 0.0, -1.0), 0.5, material_left) });
-    try world.objects.append(hittable{ .sphere = sphere.init(point3.init(-1.0, 0.0, -1.0), 0.4, material_bubble) });
-    try world.objects.append(hittable{ .sphere = sphere.init(point3.init(1.0, 0.0, -1.0), 0.5, material_right) });
+    try helper.random_init();
+
+    var a: i16 = -11;
+    while (a < 11) : (a += 1) {
+        var b: i16 = -11;
+        while (b < 11) : (b += 1) {
+            const choose_mat: f64 = helper.random_double();
+            const center: point3 = point3.init(@as(f64, @floatFromInt(a)) + 0.9 * helper.random_double(), 0.2, @as(f64, @floatFromInt(b)) + 0.9 * helper.random_double());
+
+            if (center.sub(point3.init(4, 0.2, 0)).length() > 0.9) {
+                var sphere_material: material.material = undefined;
+                if (choose_mat < 0.8) {
+                    const albedo: color.color = color.color.random().mul(color.color.random());
+                    sphere_material = material.material{ .lambertian = material.lambertian.init(albedo) };
+                } else if (choose_mat < 0.95) {
+                    const albedo: color.color = color.color.random_range(0.5, 1);
+                    const fuzz: f64 = helper.random_double_range(0, 0.5);
+                    sphere_material = material.material{ .metal = material.metal.init(albedo, fuzz) };
+                } else {
+                    sphere_material = material.material{ .dielectric = material.dielectric.init(1.5) };
+                }
+                try world.objects.append(hittable{ .sphere = sphere.init(center, 0.2, sphere_material) });
+            }
+        }
+    }
+
+    const material1: material.material = material.material{ .dielectric = material.dielectric.init(1.5) };
+    try world.objects.append(hittable{ .sphere = sphere.init(point3.init(0, 1, 0), 1.0, material1) });
+
+    const material2: material.material = material.material{ .lambertian = material.lambertian.init(color.color.init(0.4, 0.2, 0.1)) };
+    try world.objects.append(hittable{ .sphere = sphere.init(point3.init(-4, 1, 0), 1.0, material2) });
+
+    const material3: material.material = material.material{ .metal = material.metal.init(color.color.init(0.7, 0.6, 0.5), 0.0) };
+    try world.objects.append(hittable{ .sphere = sphere.init(point3.init(4, 1, 0), 1.0, material3) });
 
     var cam: camera = undefined;
 
     cam.aspect_ratio = 16.0 / 9.0;
-    cam.image_width = 400;
-    cam.samples_per_pixel = 100;
+    cam.image_width = 1200;
+    cam.samples_per_pixel = 500;
     cam.max_depth = 50;
 
     cam.vfov = 20;
-    cam.lookfrom = point3.init(-2, 2, 1);
-    cam.lookat = point3.init(0, 0, -1);
+    cam.lookfrom = point3.init(13, 2, 3);
+    cam.lookat = point3.init(0, 0, 0);
     cam.vup = vec3.init(0, 1, 0);
 
-    cam.defocus_angle = 10.0;
-    cam.focus_dist = 3.4;
+    cam.defocus_angle = 0.6;
+    cam.focus_dist = 10.0;
 
     try cam.render(world);
 }
